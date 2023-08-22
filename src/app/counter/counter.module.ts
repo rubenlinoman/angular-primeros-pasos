314073428b86f91dd8427b5d497c1dd6c5a9674c
Rubén Lino <rubenlinomandado@gmail.com>
2023-08-22 14:25:45 +0200

    Exercicio 60 feito

---
+++ src/app/counter/counter.module.ts
@@ -0,0 +1,13 @@
+import { NgModel } from "@angular/forms";
+import { CounterComponent } from "./components/counter/counter.component";
+import { NgModule } from "@angular/core";
+
+@NgModule({
+  declarations: [
+    CounterComponent
+  ],
+  exports: [
+    CounterComponent
+  ]
+})
+export class CounterModule {}
